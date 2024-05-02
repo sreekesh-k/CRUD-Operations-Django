7c9ea78ac86736fa7782533daefb1482c41bbe1f
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-02 15:37:39 +0530

    model and migrations

--- supportApp/models.py
+++ supportApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class ItemsInfo(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    quantity = models.IntegerField()
