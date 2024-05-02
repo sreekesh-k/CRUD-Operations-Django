93e1ebac08f579e10aca58c4aa0f11329250045d
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-02 15:59:12 +0530

    Item Creation

--- supportApp/views.py
+++ supportApp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from  . models import ItemsInfo
 # Create your views here.
 
 def listItems(request):
@@ -11,4 +12,10 @@ def edit(request):
 
 
 def create(request):
+    if request.POST:
+       name = request.POST.get("name")
+       description = request.POST.get("description")
+       quantity = request.POST.get("quantity") 
+       newItem = ItemsInfo(name=name,description=description,quantity=quantity)
+       newItem.save()
     return render(request,"create.html")
