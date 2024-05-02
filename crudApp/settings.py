8b44728fb01b8d468da7ced0808813712a9a291f
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-05-02 15:09:06 +0530

    templates

--- crudApp/settings.py
+++ crudApp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'crudApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
